9443c6b8670dfae740bcc2edbc0399064a7cf5a7
MalithaPrabhashana <malithaprabhashana@gmail.com>
2025-03-12 23:44:10 +0530

    added package installing loading

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -7,10 +7,8 @@ const App = () => {
   const [code, setCode] = useState("with entry { \n\tprint('Welcome to Jac'); \n}");
   const [output, setOutput] = useState("");
   const [error, setError] = useState("");
-  const [inputs, setInputs] = useState([]);
-  const [inputMode, setInputMode] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [inputVars, setInputVars] = useState([]);
+  const [loaded, setloaded] = useState(false);
   const [pyodide, setPyodide] = useState(null);
 
 
@@ -18,27 +16,12 @@ const App = () => {
     registerJacSuggestions(monaco); // Register Jac Suggestions
   };
 
-  useEffect(() => {
-    // Extract input variable names from the code
-    const inputRegex = /(\w+)\s*(?::\s*(int|float|str)\s*)?(\s*=\s*(int|float|str)?\s*\(?input\(\)\)?)/g;
-    let matches;
-    const vars = [];
-    while ((matches = inputRegex.exec(code)) !== null) {
-      const inputVar = matches[1].trim();
-      if (inputVar && !vars.includes(inputVar)) {
-        vars.push(inputVar);
-      }
-    }
-
-    setInputVars(vars.map((varName) => ({ name: varName, value: "", type: "str" })));
-  }, [code]);
-
 
   // Run event Ctrl + Enter
   useEffect(() => {
     const handleKeyDown = (event) => {
       if (event.ctrlKey && event.key === 'Enter') {
-        runCode();
+        runJacCode();
       }
     };
     window.addEventListener('keydown', handleKeyDown);
@@ -47,20 +30,6 @@ const App = () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
   }, []);
-
-
-  const runCode = async () => {
-    setOutput("");
-    setError("");
-    setLoading(true);
-
-    if (code.includes("input(")) {
-      setInputMode(true);
-      setLoading(false);
-      return;
-    }
-    await executeCode(inputs);
-  };
 
 
   useEffect(() => {
@@ -81,6 +50,7 @@ const App = () => {
             `);
 
         setPyodide(pyodideInstance);
+        setloaded(true);
       } catch (error) {
         console.error('Error loading Pyodide or JacLang:', error);
       } finally {
@@ -89,9 +59,19 @@ const App = () => {
     };
 
     if (!pyodide) {
-      loadPyodideAndJacLang();      
+      loadPyodideAndJacLang();
     }
   }, []);
+
+
+  useEffect(() => {
+    if (loaded) {
+      setTimeout(() => {
+        setloaded(false);
+        setLoading(false);
+      }, 1500);
+    }
+  }, [loaded])
 
 
   // Function to handle running JacLang code
@@ -195,72 +175,30 @@ captured_output.getvalue()
         </div>
       </div>
 
-      {/* Input Mode Popup */}
-      {inputMode && (
+      {loading && (
         <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-70 backdrop-blur-md z-50">
           <div className="bg-[#1e1e1e] text-white p-6 rounded-2xl shadow-2xl w-full max-w-md border border-gray-700">
-            <h3 className="text-xl font-semibold mb-4 text-center">Enter Inputs</h3>
-
-            {inputVars.map((inputVar, index) => (
-              <div key={index} className="mb-4">
-                <div className="flex items-center space-x-3">
-                  {/* Dropdown for selecting input type */}
-                  <select
-                    className="p-2 bg-gray-800 text-white border border-gray-700 rounded-lg focus:border-blue-500 focus:ring-2 focus:ring-blue-500 outline-none"
-                    onChange={(e) => {
-                      const updatedInputVars = [...inputVars];
-                      updatedInputVars[index].type = e.target.value; // Update type for the specific input
-                      setInputVars(updatedInputVars);
-                    }}
-                    value={inputVar.type}
-                  >
-                    <option value="str">Str</option>
-                    <option value="int">Int</option>
-                    <option value="float">Float</option>
-                  </select>
-
-                  {/* Text input field */}
-                  <input
-                    type={inputVar.type}
-                    className="w-full p-3 rounded-lg bg-gray-800 text-white border border-gray-700 focus:border-blue-500 focus:ring-2 focus:ring-blue-500 outline-none"
-                    placeholder={`Enter the value of ${inputVar.name}`}
-                    value={inputVar.value}
-                    onChange={(e) => {
-                      const updatedInputVars = [...inputVars];
-                      updatedInputVars[index].value = e.target.value; // Update value for the specific input
-                      setInputVars(updatedInputVars);
-                    }}
-                  />
-                </div>
-              </div>
-            ))}
-            <div className="flex justify-between mt-4">
-              {/* Cancel Button */}
-              <button
-                className="w-1/3 bg-red-600 text-white py-2 rounded-lg hover:bg-red-700 transition"
-                onClick={() => setInputMode(false)}
-              >
-                Close
-              </button>
-
-              {/* Run Button */}
-              <button
-                className="w-2/3 bg-green-600 text-white py-2 rounded-lg hover:bg-green-700 transition ml-4"
-                onClick={() => {
-                  const formattedInputs = inputVars.map(input => `${input.value}:${input.type}`);
-                  setInputs(formattedInputs); // Set the inputs to be used when executing the code
-                  executeCode(formattedInputs); // Run code with all inputs
-                }}
-              >
-                Run Code
-              </button>
+            <h3 className="text-xl font-semibold mb-4 text-center">Packages are installing...</h3>
+            <div className="flex justify-center items-center">
+              <div className="w-12 h-12 border-t-4 border-blue-600 border-solid rounded-full animate-spin"></div>
             </div>
-
-
           </div>
         </div>
       )}
 
+      {loaded && !loading && (
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-70 backdrop-blur-md z-50">
+          <div className="bg-[#1e1e1e] text-white p-6 rounded-2xl shadow-2xl w-full max-w-md border border-gray-700">
+            <h3 className="text-xl font-semibold mb-4 text-center">Packages successfully installed!</h3>
+            <div className="flex justify-center items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" className="w-12 h-12 text-green-500">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
+              </svg>
+            </div>
+            <p className="text-center mt-2">Play with JacLang now!</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
